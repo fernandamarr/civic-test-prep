552d6927443606c53ff2a315b1c82c38e6349785
Fernanda <fernanda.marroquin07@gmail.com>
2019-08-07 18:54:12 -0400

    adds basic layout of game

--- src/responses.js
+++ src/responses.js
@@ -2,6 +2,9 @@
 
 'use strict';
 
+module.exports.gamePrelude = () => 
+  'This is a quiz game to prepare you for the U.S. Civics Test. Do you want to play?'
+
 module.exports.keepGoing = () =>
   'Would you like to keep going?';
 
